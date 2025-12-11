48ebdb33171faaf7a515a6d6cabd60ad2ecabc35
Shrey Nagda <shreynagda2714@gmail.com>
2025-12-11 13:25:51 +0530

    Updated app url

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,7 +2,7 @@ export const APP_NAME = "StaticSend";
 export const APP_DESCRIPTION =
   "The missing backend for your static forms. Secure, reliable, and easy to setup form endpoints for developers.";
 export const APP_URL =
-  process.env.NEXT_PUBLIC_APP_URL || "https://staticsend.com";
+  process.env.NEXTAUTH_URL || "https://staticsend.vercel.app";
 
 export const SOCIAL_LINKS = {
   twitter: "https://x.com/shrey_nagda",
